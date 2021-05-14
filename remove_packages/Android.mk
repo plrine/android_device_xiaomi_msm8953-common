1eb2ca97bcd4c19214c8bc109ebc70693f532c34
Lunarixus <Nathanb644@hotmail.co.uk>
2021-05-14 19:23:42 +0000

    msm8953-common: Remove unwanted GApps Packages
    Change-Id: Ie4f08459a6004bb16d7dfec7e7a152a42dc0ffdf
    Signed-off-by: RayapKun <rayapkun@gmail.com>
    Signed-off-by: frost. (frostg-012) <frost@nusantararom.org>

---
+++ remove_packages/Android.mk
@@ -0,0 +1,24 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := RemovePackages
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_TAGS := optional
+LOCAL_OVERRIDES_PACKAGES := \
+    Maps \
+    MarkupGoogle \
+    TurboPrebuilt \
+    Velvet \
+    SafetyHubPrebuilt \
+    ScribePrebuilt \
+    EmergencyInfoGoogleNoUi \
+    Flipendo \
+    talkback \
+    AICorePrebuilt \
+    AiWallpapers \
+    PrebuiltGmail \
+    SoundAmplifierPrebuilt
+LOCAL_UNINSTALLABLE_MODULE := true
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_SRC_FILES := /dev/null
+include $(BUILD_PREBUILT)
