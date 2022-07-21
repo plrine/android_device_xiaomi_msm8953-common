b28068c491967ccdacca2d47f3a32f431be6df79
me-cafebabe <me_cafebabe@hotmail.com>
2022-07-22 01:42:26 +0530

    msm8953-common: Setup Retrofit Dynamic Partitions
    [rktdnt]: Adapt with A/B changes for tissot
    
    Co-authored-by: Raya Mahardika <rayapkun@gmail.com>
    Change-Id: I5451984b5b5261e62e5dac35605922278504d3ce

--- msm8953.mk
+++ msm8953.mk
@@ -16,6 +16,10 @@ PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
+# Partitions
+PRODUCT_RETROFIT_DYNAMIC_PARTITIONS := true
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
@@ -60,6 +64,15 @@ AB_OTA_POSTINSTALL_CONFIG += \
 
 PRODUCT_PACKAGES += \
     otapreopt_script
+
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_product=true \
+    POSTINSTALL_PATH_product=bin/check_dynamic_partitions \
+    FILESYSTEM_TYPE_product=ext4 \
+    POSTINSTALL_OPTIONAL_product=false
+
+PRODUCT_PACKAGES += \
+    check_dynamic_partitions
 
 # Boot control
 PRODUCT_PACKAGES += \
