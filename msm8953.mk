0b9b3006016e52f343a330e5eaec4c193fb0d1fd
Raya Mahardika <rayapkun@hlcyn.co>
2024-09-20 16:36:14 +0700

    msm8953-common: Build libprotobuf-cpp
    Change-Id: I7cb843bb3c8d37c354e7fc166060b9f1e6579920

--- msm8953.mk
+++ msm8953.mk
@@ -392,6 +392,12 @@ PRODUCT_PACKAGES += \
     android.hardware.usb@1.3-service.basic
 
 # VNDK
+PRODUCT_PACKAGES += \
+    libprotobuf-cpp-full-vendorcompat \
+    libprotobuf-cpp-lite-vendorcompat \
+    libprotobuf-cpp-full-3.9.1-vendorcompat \
+    libprotobuf-cpp-lite-3.9.1-vendorcompat
+
 PRODUCT_COPY_FILES += \
     prebuilts/vndk/v33/arm64/arch-arm64-armv8-a/shared/vndk-sp/libutils.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libutils-v33.so
 
