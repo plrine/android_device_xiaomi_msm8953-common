dc510972284cc5f5411a9f1711635efe36cd475e
Giovanni Ricca <giovanniricca@proton.me>
2024-11-29 16:21:18 +0100

    msm8953-common: Decommonize screen density
    Change-Id: I0c2c2723c0e9826c549e86dd591bc64893ce98cf

--- msm8953.mk
+++ msm8953.mk
@@ -10,10 +10,6 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
 # APEX
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
-# Screen density
-PRODUCT_AAPT_CONFIG := normal
-PRODUCT_AAPT_PREF_CONFIG := xxhdpi
-
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
