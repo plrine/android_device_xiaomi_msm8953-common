35fdc384572e68da64e6bcba83f946874e055d90
ExtremeXT <extremext@lineageos.org>
2026-07-03 18:06:20 +0300

    msm8953-common: Set legacy gralloc flag
    * We don't have gralloc 4 or higher
    
    Change-Id: Ie7fb53bb16284df42fd7c3409710877d37ce58a5

--- msm8953.mk
+++ msm8953.mk
@@ -97,6 +97,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service
 
 # Display
+$(call soong_config_set_bool,libui,legacy_gralloc,true)
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
     android.hardware.graphics.allocator@2.0-service \
