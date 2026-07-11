49815cfa2aef0e277cbc877789539f6671de62dc
Nolen Johnson <johnsonnolen@gmail.com>
2026-07-11 18:16:27 +0000

    msm8953-common: Stop disabling configstore
    * As of CP2A it's just dead and gone.
    
    Change-Id: Ie73368e5e3e1e3295f0cdf47540e4b8825440f7b
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- msm8953.mk
+++ msm8953.mk
@@ -96,10 +96,6 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service
 
-# Configstore
-PRODUCT_PACKAGES += \
-    disable_configstore
-
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
