36df14549f65ad94e82947548b3899dbc3536572
Raya Mahardika <94042378+rktdnt@users.noreply.github.com>
2026-09-01 14:37:47 +0000

    msm8953-common: Update Radio & QMI blobs from LA.UM.9.6.3.r1-06200-89xx.0
    Change-Id: Ia05ded706936f69e987978d1e158104030ba27c8

--- extract-files.py
+++ extract-files.py
@@ -36,10 +36,11 @@ lib_fixups: lib_fixups_user_type = {
     **lib_fixups,
     (
         'com.qualcomm.qti.dpm.api@1.0',
+        'vendor.qti.ims.callcapability@1.0',
         'vendor.qti.ims.callinfo@1.0',
         'vendor.qti.ims.rcsconfig@1.0',
-        'vendor.qti.imsrtpservice@2.0',
-        'vendor.qti.imsrtpservice@2.1',
+        'vendor.qti.ims.rcsconfig@2.0',
+        'vendor.qti.ims.rcsconfig@2.1',
     ): lib_fixup_vendor_suffix,
     ('libwifi-hal-ctrl'): lib_fixup_remove,
 }
@@ -52,7 +53,7 @@ blob_fixups: blob_fixups_user_type = {
     ('system_ext/lib64/lib-imscamera.so', 'system_ext/lib64/lib-imsvideocodec.so'): blob_fixup()
         .add_needed('libgui_shim.so'),
     'vendor/bin/pm-service': blob_fixup()
-        .add_needed('libutils-v33.so'),
+        .replace_needed('libutils.so', 'libutils-v33.so'),
     ('vendor/etc/data/dsi_config.xml', 'vendor/etc/data/netmgr_config.xml'): blob_fixup()
         .fix_xml(),
     ('vendor/lib64/mediadrm/libwvdrmengine.so', 'vendor/lib64/libwvhidl.so'): blob_fixup()
--- msm8953.mk
+++ msm8953.mk
@@ -252,6 +252,7 @@ PRODUCT_PACKAGES += \
     ueventd.qcom.rc
 
 PRODUCT_PACKAGES += \
+    init.class_main.sh \
     init.qcom.sh \
     init.qcom.post_boot.sh
 
